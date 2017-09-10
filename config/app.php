6e189ccca0df2c944fb6426b20ed197a57bdddb6
Konata69 <ikutoviolin>
2017-09-10 22:46:23 +0300

    laravel ide helper added package.json changed

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * My providers
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
