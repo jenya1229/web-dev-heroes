40f8c6003445c70292f0944796fea765e36954f6
Konata69 <ikutoviolin>
2017-09-11 22:31:00 +0300

    laravel auth

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
          * My providers
          */
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -230,6 +231,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
